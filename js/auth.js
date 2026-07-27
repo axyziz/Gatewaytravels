4d76bd325cb327496a6bd120da975f6d925549ce
axyziz <8iazizi8@gmail.com>
2026-07-27 22:15:26 +0100

    auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,15 @@
+async function checkLogin() {
 
+    const { data, error } = await supabaseClient.auth.getSession();
+
+    if (error) {
+        console.error(error);
+        window.location.href = "login.html";
+        return;
+    }
+
+    if (!data.session) {
+        window.location.href = "login.html";
+    }
+
+}
