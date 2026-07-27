dad0d30a0872b3696df8b0891201460174a17386
axyziz <8iazizi8@gmail.com>
2026-07-27 21:41:42 +0100

    supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
 
+const SUPABASE_URL = "https://ajlkweolggmgmbikqfhe.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_ytDyn-0nK2uNvcBuIl4XOg_Yay4H5FI";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
